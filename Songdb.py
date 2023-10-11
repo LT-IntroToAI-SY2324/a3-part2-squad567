27544437c303c6c3891198fa79bbf8a826b91027
Lillie Mackey <lamackey@cps.edu>
2023-10-11 10:40:48 -0500

    Pitbull song push

--- Songdb.py
+++ Songdb.py
@@ -2,7 +2,17 @@ song_db: List[Tuple[str, List[str], List[str]]]=[
     (
         "Pitbull", #artist
         [
-            ""
+            "Planet Pit",
+            "globalization",
+            "Global Warming"
+        ],
+        [
+            "Internation Love",
+            "Time of Our Lives",
+            "Timber"
         ]
+    ),
+    (
+        ""
     )
 ]
