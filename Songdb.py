a9a1521e62ae1ebacd694779655a256e1455b2c2
Lillie Mackey <lamackey@cps.edu>
2023-10-12 10:21:34 -0500

    Added lots of artitsts

--- Songdb.py
+++ Songdb.py
@@ -5,14 +5,186 @@ song_db: List[Tuple[str, List[str], List[str]]]=[
             "Planet Pit",
             "globalization",
             "Global Warming"
-        ],
+        ],#Top Albums
         [
             "Internation Love",
             "Time of Our Lives",
             "Timber"
+        ],#Top Songs
+    ),
+    (
+        "Justin Timberlake",
+        [
+            "FutureSex/LoveSounds",
+            "Justified",
+            "The 20/20 Experience",
+        ],
+        [
+            "Mirrors",
+            "Wht Goes Around Comes Around",
+            "My Love",
+        ],
+    ),
+    (
+        "Kanye West",
+        [
+            "graduation",
+            "The College Dropout",
+            "The Life Of Pablo",
+        ],
+        [
+            "Good Life",
+            "Runaway",
+            "Stronger",
+        ],
+    ),
+    (
+        "Bad Bunny",
+        [
+            "Un Verano Sin Ti",
+            "El Ultimo Tour Del Mundo",
+            "YHLQMDLG",
+        ],
+        [
+            "I Like it",
+            "Me Porto Bonito",
+            "Titi Me Pregunto",
+        ],
+    ),
+    (
+        "SZA",
+        [
+            "SOS",
+            "Ctrl",
+            "Z",
+        ],
+        [
+            "Love Galore",
+            "Good Days",
+            "Kill Bill",
+        ],
+    ),
+    (
+        "Maroon 5",
+        [
+            "Overexposed",
+            "Songs About Jane",
+            "V",
+        ],
+        [
+            "Girls Like You",
+            "Memories",
+            "PayPhone",
+        ],
+    ),
+    (
+        "Justin Beiber",
+        [
+            "Changes",
+            "Purpose",
+            "Believe",
+        ],
+        [
+            "Despacito",
+            "I'm The One",
+            "Intentions",
+        ],
+    ),
+    (
+        "Black Eyed Peas",
+        [
+            "The Beginning",
+            "Monkey Buisness",
+            "The Time",
+        ],
+        [
+            "My Humps",
+            "Pump It",
+            "Just Can't Get Enough",
+        ],
+    ),
+    (
+        "Eminem",
+        [
+            "Encore",
+            "Relapse: Refil",
+            "Recovery",
+        ],
+        [
+            "Till I Colapse",
+            "The Real Slim Shady",
+            "Forgot About Dre",
+        ],
+    ),
+    (
+        "The Weeknd",
+        [
+            "Starboy",
+            "After Hours",
+            "Dawn FM",
+        ],
+        [
+            "StarBoy",
+            "Die For You",
+            "Low Life",
+        ],
+    ),
+    (
+        "Kendrick Lamar",
+        [
+            "DAMN",
+            "Mr.Morale & The Big Steppers",
+            "To Pimp a Butterfly",
+        ],
+        [
+            "Humble",
+            "Love",
+            "DNA",
+        ],
+    ),
+    (
+        "J. Cole",
+        [
+            " 2014 Forest Hills Drive",
+            "KOD",
+            "Revenge of the Dreamers",
+        ],
+        [
+            "No role Modelz",
+            "MIDDLE CHILD",
+            "Power Trip",
+        ],
+    ),
+    (
+        "Future",
+        [
+            "I NEVER LIKED YOU",
+            "High Off Life",
+            "SAVE ME"
+        ],
+        [
+            "X",
+            "WAIT FOR U",
+            "Life is Good",
+        ],
+    ),
+    (
+        "Chris Brown",
+        [
+            "Slime & B",
+            "X",
+            "Indigo",
+        ],
+        [
+            "No Guidance",
+            "Go Crazy",
+            "Under the Influence",
         ]
     ),
     (
-        ""
+        "Rhianna",
+        [
+            ""
+        ]
     )
 ]
